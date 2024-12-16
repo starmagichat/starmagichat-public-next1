b1fd7821f1ba464cf792ae4f4e466e5cd454931b
star Magic hat <154031802+starmagichat@users.noreply.github.com>
2024-12-16 20:59:17 +0900

    pnpmでshadcnも導入しました。buttonはついで

---
+++ app/test/starmagichat/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+
+const page = () => {
+    return (
+        <div>
+            <Button>click?</Button>
+        </div>
+    )
+}
+
+export default page
