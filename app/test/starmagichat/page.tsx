dce2744dbedefdec5cf5ff1e9ba2de2f53d26b21
star Magic hat <154031802+starmagichat@users.noreply.github.com>
2024-12-21 11:35:54 +0900

    promiseのやりかたを学習

--- app/test/starmagichat/page.tsx
+++ app/test/starmagichat/page.tsx
@@ -6,31 +6,38 @@ export default async function Page() {
     // const cookieStore = await cookies()
     // const supabase = createClient(cookieStore)
 
-    try {
-        const { data: todos, error } = await supabase
-            .from('todos')
-            .select();
+    async function hoge(): Promise<any[]> {
+        try {
+            const { data: todos, error } = await supabase
+                .from('todos')
+                .select();
 
-        if (error) {
-            throw error; // エラーがあれば例外を投げる
+            if (error) {
+                throw error; // エラーがあれば例外を投げる
+            }
+
+            console.log(todos); // 取得したデータを確認
+            const returnI = todos
+            return returnI
+
+        } catch (error: any) {
+            console.error('Error fetching todos:', error.message); // エラーメッセージを表示
+            return [];
         }
-
-        console.log(todos); // 取得したデータを確認
-
-    } catch (error) {
-        console.error('Error fetching todos:', error.message); // エラーメッセージを表示
     }
+
+
 
 
 
     return (
         <div>
             <div>aa</div>
-            <ul>
-                {todos?.map((todo) => (
+            {/* <ul>
+                {hoge.map((todo) => (
                     <li key={todo.id}>{todo}</li>
                 ))}
-            </ul>
+            </ul> */}
         </div>
     )
 }
